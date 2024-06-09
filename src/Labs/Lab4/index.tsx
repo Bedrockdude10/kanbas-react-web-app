20f774f7540f830b87f86b3802078c947356e3b1
bedrockdude10 <dannyrollo4@gmail.com>
2024-06-09 17:05:21 -0400

    thru 2.4.3

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,12 @@
+import ClickEvent from "./clickEvent";
+import ReduxExamples from "./ReduxExamples";
+
+export default function Lab4() {
+    return(
+        <div id="wd-lab4" className="container-fluid">
+          <ClickEvent/>
+
+          <ReduxExamples/>
+        </div>
+      );
+}
