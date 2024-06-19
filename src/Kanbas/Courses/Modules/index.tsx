9258d40ae2f505788a23d422630fec47778a0c11
bedrockdude10 <dannyrollo4@gmail.com>
2024-06-19 00:26:44 -0400

    Done up to 3.4

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,13 +1,12 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import ModulesControls from "./ModulesControls";
 import ModuleControlButtons from "./ModuleControlButtons";
 import LessonControlButtons from "./LessonControlButtons";
 import { BsGripVertical } from "react-icons/bs";
 import { useParams } from "react-router";
-import * as db from "../../Database";
-import { addModule, editModule, updateModule, deleteModule }
-  from "./reducer";
+import { addModule, editModule, updateModule, deleteModule } from "./reducer";
 import { useSelector, useDispatch } from "react-redux";
+import './index.css';
 
 export default function Modules() {
   const { cid } = useParams();
@@ -15,39 +14,47 @@ export default function Modules() {
   const { modules } = useSelector((state: any) => state.modulesReducer);
   const dispatch = useDispatch();
 
-return (
+  return (
     <div className="wd-modules">
-      <ul id="wd-modules" className="list-group rounded-0">
-      <ModulesControls moduleName={moduleName} setModuleName={setModuleName}
+      <ModulesControls 
+        moduleName={moduleName} 
+        setModuleName={setModuleName}
         addModule={() => {
           dispatch(addModule({ name: moduleName, course: cid }));
           setModuleName("");
-        }}        
-        {modules
-          .filter((module) => module.course === cid)
-          .map((module: any) => (
-            <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-              {!module.editing && module.name}
-              { module.editing && (
-                <input className="form-control w-50 d-inline-block"
-                      onChange={(e) => updateModule({ ...module, name: e.target.value })}
-                      onKeyDown={(e) => {
-                        if (e.key === "Enter") {
-                          updateModule({ ...module, editing: false });
-                        }
-                      }}
-                      value={module.name}/>
-              )}
-              <ModuleControlButtons 
-                moduleId={module._id} 
-                deleteModule={deleteModule}
-                editModule={editModule}/>
-
-            </li>
-          ))}
+        }}
+      />
+      <ul id="wd-modules" style={{ 
+        display: 'flex', 
+        flexDirection: 'column', 
+        width: '100%', 
+        listStyleType: 'none', 
+        padding: 0 
+      }}>
+        {modules.filter((module: any) => module.course === cid).map((module: any) => (
+          <li key={module._id} className="list-group-item"> {/* Added key and li as the wrapper */}
+            {!module.editing ? (
+              <span>{module.name}</span>
+            ) : (
+              <input 
+                className="form-control w-50 d-inline-block"
+                onChange={(e) => dispatch(updateModule({ ...module, name: e.target.value }))}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    dispatch(updateModule({ ...module, editing: false }));
+                  }
+                }}
+                value={module.name}
+              />
+            )}
+            <ModuleControlButtons 
+              moduleId={module._id}
+              deleteModule={() => dispatch(deleteModule(module._id))}
+              editModule={() => dispatch(editModule(module._id))}
+            />
+          </li>
+        ))}
       </ul>
     </div>
-);}
-
-
-
+  );
+}
