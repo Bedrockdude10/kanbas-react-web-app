a6c72f31870945e3443c982de2ab20e876017521
bedrockdude10 <dannyrollo4@gmail.com>
2024-06-22 14:37:56 -0400

    3.6.2

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -4,6 +4,8 @@ import WorkingWithObjects from "./WorkingWithObjects";
 import Modules from "./Modules";
 import WorkingWithArrays from "./WorkingWithArrays";
 import HttpClient from "./HttpClient";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function Lab5() {
@@ -22,6 +24,8 @@ export default function Lab5() {
         <Modules />
         <WorkingWithArrays/>
         <HttpClient />
+        <WorkingWithObjectsAsynchronously/>
+        <WorkingWithArraysAsynchronously/>
       </div>
     );
   }
